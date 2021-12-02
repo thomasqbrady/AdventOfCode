b9455cf9cd1dc0fa6fd3bcdbfe6f47a340dbd4ab
Brady,Thomas Q <121729+thomasqbrady@users.noreply.github.com>
2021-12-02 08:46:37 -0600

    Day 1 finished

--- 2021/day1/index.js
+++ 2021/day1/index.js
@@ -1,19 +1,25 @@
 const fs = require('fs');
 const input = fs.readFileSync('./input.txt', 'utf8');
+// const input = fs.readFileSync('./test.txt', 'utf8');
 
 let measurements = input.split('\n');
 
 let increaseCount = 0;
+let width = 3;
 
-console.log(measurements[0], measurements[measurements.length-1])
+let lastSum = 0;
 
-for (let i = 0;i < measurements.length - 1;i++) {
-// for (let i = 0;i < 10;i++) {
-  // console.log(measurements[i], measurements[i+1], measurements[i + 1] > measurements[i])
-  if (parseInt(measurements[i + 1], 10) > parseInt(measurements[i], 10)) {
+for (let i = 0; i < width; i++) {
+  lastSum += parseInt(measurements[i], 10);
+}
+
+for (let i = width;i < measurements.length;i++) {
+  let newSum = lastSum - parseInt(measurements[i - width], 10) + parseInt(measurements[i]);
+  console.log(lastSum, newSum);
+  if (newSum > lastSum) {
     increaseCount++;
-    // console.log('increase', increaseCount);
   }
+  lastSum = newSum;
 }
 
 console.log(increaseCount);
