5638b5c521c778f8f50cb1fa3cb21e8c73ab3e1f
Thomas Q Brady <tqb@thomasqbrady.com>
2021-12-02 08:59:14 -0600

    Day 2 finished

--- 2021/day2/index.js
+++ 2021/day2/index.js
@@ -1,19 +1,27 @@
 const fs = require('fs');
 const input = fs.readFileSync('./input.txt', 'utf8');
 
-let measurements = input.split('\n');
+let commands = input.split('\n');
 
-let increaseCount = 0;
+let position = { x: 0, y: 0, aim: 0};
 
-console.log(measurements[0], measurements[measurements.length-1])
 
-for (let i = 0;i < measurements.length - 1;i++) {
-// for (let i = 0;i < 10;i++) {
-  // console.log(measurements[i], measurements[i+1], measurements[i + 1] > measurements[i])
-  if (parseInt(measurements[i + 1], 10) > parseInt(measurements[i], 10)) {
-    increaseCount++;
-    // console.log('increase', increaseCount);
+for (let i = 0;i < commands.length;i++) {
+  let command = commands[i].split(' ');
+  let direction = command[0];
+  let distance = parseInt(command[1], 10);
+  switch (direction) {
+    case 'forward' :
+      position.x += distance;
+      position.y += position.aim * distance;
+      break;
+    case 'down' :
+      position.aim += distance;
+      break;
+    case 'up' :
+      position.aim -= distance;
+      break;
   }
 }
 
-console.log(increaseCount);
+console.log(position, position.x * position.y);
