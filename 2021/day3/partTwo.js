d8aeca4abe1bfe3c7353a7a782fc7fd63dc317cf
Thomas Q Brady <tqb@thomasqbrady.com>
2021-12-06 09:36:02 -0600

    day3 part 2 done

--- 2021/day3/partTwo.js
+++ 2021/day3/partTwo.js
@@ -1,59 +1,78 @@
 const fs = require('fs');
-const input = fs.readFileSync('./test.txt', 'utf8');
+const input = fs.readFileSync('./input.txt', 'utf8');
 
 let logs = input.split('\n');
 
-let counts = {}
-
-logs.forEach((log) => {
-  log.split('').forEach((bit, index) => {
-    bit = parseInt(bit,10);
-    if (!counts[index]) {
-      counts[index] = {};
+function makeMap(_logs) {
+  let map = {
+    unique: [],
+    counts: {}
+  };
+  _logs.forEach((log) => {
+    if (map.unique.indexOf(log) === -1) {
+      map.unique.push(log);
     }
-    counts[index][bit] ? counts[index][bit] += 1 : counts[index][bit] = 1;
+    log.split('').forEach((bit, index) => {
+      bit = parseInt(bit,10);
+      if (!map.counts[index]) {
+        map.counts[index] = {};
+      }
+      map.counts[index][bit] ? map.counts[index][bit] += 1 : map.counts[index][bit] = 1;
+    });
   });
-});
+  return map;
+}
 
-console.log({counts});
-
-function readLifeSupport(_logs, column, high) {
-  console.log({_logs, column, high});
-  if (column == _logs[0].length) {
-    return _logs;
+function readLifeSupport(map, column, high) {
+  // console.log({map});
+  if (column === map.unique[0].length || map.unique.length === 1) {
+    return map.unique;
   }
   let match;
   if (high) {
-    if (counts[column][0] > counts[column][1]) {
+    if (map.counts['' + column]['0'] > map.counts['' + column]['1']) {
       match = 0;
     } else {
-      if (counts[column][0] < counts[column][1]) {
+      if (map.counts['' + column]['0'] < map.counts['' + column]['1']) {
         match = 1;
       } else {
         match = 1;
       }
     }
   } else {
-    if (counts[column][0] > counts[column][1]) {
+    if (map.counts['' + column]['0'] > map.counts['' + column]['1']) {
       match = 1;
     } else {
-      if (counts[column][0] < counts[column][1]) {
+      if (map.counts['' + column]['0'] < map.counts['' + column]['1']) {
         match = 0;
       } else {
         match = 0;
       }
     }
   }
-  _logs.forEach((log, index) => {
-    let bit = parseInt(log[column], 10);
-    console.log({log, index, bit, match});
-    if (bit !== match) {
-      console.log('removing ', log, 'because ', log[column], ' does not match ', match);
-      _logs.splice(index,1);
-      counts[column][bit] -= 1;
+  // console.dir({map, match, column}, { depth: null });
+  [...map.unique].forEach((reading) => {
+    let matchBit = parseInt(reading[column], 10);
+    // console.log({reading, matchBit, match});
+    if (matchBit !== match) {
+      let index = map.unique.indexOf(reading);
+      // console.log(`removing ${reading} at index of ${index} because ${reading[column]} in column ${column } does not match ${match}`);
+      map.unique.splice(index, 1);
+      reading.split('').forEach((bit, index) => {
+        map.counts['' + index]['' + bit] -= 1;
+      });
     }
   });
-  return readLifeSupport(_logs, column + 1, high);
+  // console.log('AFTER');
+  // console.dir({map, match}, { depth: null });
+  return readLifeSupport(map, column + 1, high);
 }
 
-console.log(readLifeSupport([...logs], 0, true));
+let map = makeMap([...logs]);
+// console.log('=====================================');
+// console.log('S        T        A        R        T');
+// console.log('=====================================');
+let oxygen = parseInt(readLifeSupport(map, 0, true), 2);
+map = makeMap([...logs]);
+let co2 = parseInt(readLifeSupport(map, 0, false), 2);
+console.log({oxygen, co2}, oxygen * co2);
