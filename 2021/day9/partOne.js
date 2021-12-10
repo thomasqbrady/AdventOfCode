e9ef71e44da49b6646a9e958c479048b1d7c0a56
Brady,Thomas Q <121729+thomasqbrady@users.noreply.github.com>
2021-12-09 19:42:18 -0600

    working on part 2 day 9

--- 2021/day9/partOne.js
+++ 2021/day9/partOne.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const input = fs.readFileSync('./test.txt', 'utf8');
+const input = fs.readFileSync('./input.txt', 'utf8');
 
 console.log('===============================================');
 console.log('===  S        T        A        R        T  ===');
@@ -13,9 +13,37 @@ function outputToFile(contents, filename) {
 }
 
 let readings = input.split('\n');
-for (let row = 0;row < readings.length;row++) {
-  for (let col = 0;col < readings[row].length;col++) {
-    let reading = readings[row][col];
-    console.log({reading});
+let width = readings[0].length;
+let height = readings.length;
+let lowPoints = [];
+for (let row = 0;row < height;row++) {
+  for (let col = 0;col < width;col++) {
+    let reading = parseInt(readings[row][col], 10);
+    let up = 10, right = 10, down = 10, left = 10;
+    if (row > 0) {
+      up = parseInt(readings[row - 1][col], 10);
+    }
+    if (col < width - 1) {
+      right = parseInt(readings[row][col + 1], 10);
+    }
+    if (row < height - 1) {
+      down = parseInt(readings[row + 1][col], 10);
+    }
+    if (col > 0) {
+      left = parseInt(readings[row][col - 1], 10);
+    }
+    let lowest = true;
+    [up, right, down, left].forEach((neighbor) => {
+      if (reading >= neighbor) {
+        lowest = false;
+      }
+    });
+    if (lowest) {
+      lowPoints.push(reading + 1);
+    }
   }
 };
+
+console.log(lowPoints, lowPoints.reduce((accum, reading) => {
+  return accum + reading
+}));
--- 2021/day9/partTwo.js
+++ 2021/day9/partTwo.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const input = fs.readFileSync('./input.txt', 'utf8');
+const input = fs.readFileSync('./test.txt', 'utf8');
 
 console.log('===============================================');
 console.log('===  S        T        A        R        T  ===');
@@ -12,5 +12,62 @@ function outputToFile(contents, filename) {
   });
 }
 
-let signals = input.split('\n');
+let readings = input.split('\n');
+let width = readings[0].length;
+let height = readings.length;
 
+let basins = {}
+
+function exploreBasin(checkPosition, basin) {
+  console.log('checking',checkPosition.x,checkPosition.y);
+  if (!checkPosition.reading) {
+    checkPosition.reading = parseInt(readings[checkPosition.y][checkPosition.x]);
+  }
+  let up, right, down, left;
+  let x = checkPosition.x;
+  let y = checkPosition.y;
+  basin[`${x}:${y}`] = true;
+  if (y > 0) {
+    up = {
+      reading: parseInt(readings[y - 1][x], 10),
+      x: x,
+      y: y - 1
+    };
+  }
+  if (x < width - 1) {
+    right = {
+      reading: parseInt(readings[y][x + 1], 10),
+      x: x + 1,
+      y: y
+    };
+  }
+  if (y < height - 1) {
+    down = {
+      reading: parseInt(readings[y + 1][x], 10),
+      x: x,
+      y: y + 1
+    };
+  }
+  if (x > 0) {
+    left = {
+      reading: parseInt(readings[y][x - 1], 10),
+      x: x - 1,
+      y: y
+    };
+  }
+  console.log(up, right, down, left);
+  [up, right, down, left].forEach((position) => {
+    if (position && position.reading !== 9 && !basin[`${position.x}:${position.y}`]) {
+      exploreBasin(position);
+    }
+  })
+}
+for (let row = 0;row < 1;row++) {
+  for (let col = 0;col < width;col++) {
+    if (parseInt(readings[row][col], 10) !== 9) {
+      let newBasinID = Object.keys(basins).length;
+      basins[newBasinID] = {};
+      exploreBasin
+    }
+  }
+}
