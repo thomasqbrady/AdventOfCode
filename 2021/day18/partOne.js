80290b93e86cec84d6e41bb4cc5a5c360d73ac37
Brady,Thomas Q <121729+thomasqbrady@users.noreply.github.com>
2021-12-21 15:53:14 -0600

    half-hearted attempts at 18 and 19

--- 2021/day18/partOne.js
+++ 2021/day18/partOne.js
@@ -11,25 +11,76 @@ function outputToFile(contents, filename) {
   });
 }
 
-let numbers = input.split('\n');
-
-console.log(numbers);
-
-function explodePass(number) {
-  let nestLevel = 0;
-  for (let i = 0;i < number.length;i++) {
-    if (number[i] === '[') {
-      nestLevel += 1;
-      if (nestLevel === 5) {
-        console.log(number[i + 1]);
+function findRecipient(pairMap, add, direction) {
+  if (direction === 'l' && (typeof pairMap.left !== 'object')) ||  {
+    pairMap.left += add;
+  } else {
+    if (direction === 'r' && (typeof pairMap.right !== 'object')) ||  {
+      pairMap.right += add;
+    } else {
+      if (pairMap.parent) {
+        findRecipient(pairMap.parent, add, direction);
       }
-    }
-    if (number[i] === ']') {
-      nestLevel -= 1;
     }
   }
 }
 
-numbers.forEach((number) => {
-  explodePass(number);
-})
+function findMine(pairMap, level = 0, parent = null) {
+  // console.log({pairMap, level, parent});
+  if (parent) {
+    pairMap.parent = parent;
+  }
+  if (typeof pairMap !== 'object') {
+    return null;
+  }
+  if (level === 4) {
+    if (parent.left === pairMap) {
+      let addLeft = pairMap.left;
+      let addRight = pairMap.right;
+      parent.left = 0;
+      
+      // console.log('replace left');
+    }
+    if (parent.right === pairMap) {
+      parent.right = 0;
+      // console.log('replace right');
+    }
+  } else {
+    let mine = findMine(pairMap.left, level + 1, pairMap);
+    if (mine) {
+      return mine
+    } else {
+      return findMine(pairMap.right, level + 1, pairMap);
+    }
+  }
+}
+
+input.split('\n').forEach((number) => {
+  console.log(number);
+  let numberObjString = number.replace(/\[/g, '{"left": ');
+  numberObjString = numberObjString.replace(/,/g, ',"right": ');
+  numberObjString = numberObjString.replace(/\]/g, '}');
+  // console.log({numberObjString})
+  // let nestLevel = -1;
+  // for (let i = 0;i < numberObjString.length;i++) {
+  //   if (numberObjString[i] === '{') {
+  //     nestLevel += 1;
+  //   }
+  //   if (numberObjString[i] === '}') {
+  //     numberObjString = numberObjString.substring(0, i) + ',"level":' + nestLevel + numberObjString.substring(i, numberObjString.length);
+  //     i += 9;
+  //     i += String(nestLevel).length;
+  //     nestLevel -= 1;      
+  //   }
+  // }
+  // console.log({numberObjString});
+  let numberObj = JSON.parse(numberObjString);
+  // console.dir(numberObj, {depth: null});
+
+  let mine = findMine(numberObj);
+  // while (mine) {
+
+  // }
+  console.dir(numberObj, {depth: null});
+});
+
---
+++ 2021/day19/partOne.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const input = fs.readFileSync('./test.txt', 'utf8');
+
+console.log('===============================================');
+console.log('===  S        T        A        R        T  ===');
+console.log('===============================================');
+
+function outputToFile(contents, filename) {
+  fs.writeFile(filename, JSON.stringify(contents), (err) =>{
+    console.error(err);
+  });
+}
+
