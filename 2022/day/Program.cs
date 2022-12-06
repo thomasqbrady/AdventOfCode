26dd00a736c5ce4530f71204944790201d06df74
Thomas Q Brady <tqb@thomasqbrady.com>
2022-12-06 14:48:22 -0600

    through day 6

---
+++ 2022/day/Program.cs
@@ -0,0 +1,30 @@
+﻿/** author: Thomas Q Brady
+*** Advent of Code 2022 day 1
+**/
+
+namespace com.thomasqbrady
+{
+    class Day
+    {
+        static void Main()
+        {
+            PartOne();
+            // PartTwo();
+        }
+        static void PartOne()
+        {
+            string input = System.IO.File.ReadAllText(@"test.txt");
+            // string input = System.IO.File.ReadAllText(@"input.txt");
+            Console.WriteLine("Input:\n{0}", input);
+            Console.WriteLine("===========");
+        }
+
+        static void PartTwo()
+        {
+            string input = System.IO.File.ReadAllText(@"test.txt");
+            // string input = System.IO.File.ReadAllText(@"input.txt");
+            Console.WriteLine("Input:\n{0}", input);
+            Console.WriteLine("===========");
+        }
+    }
+}
--- 2022/day6/Program.cs
+++ 2022/day6/Program.cs
@@ -4,78 +4,54 @@
 
 namespace com.thomasqbrady
 {
-    class Day5
+    class Day6
     {
-        public static IDictionary<string, Stack<string>> stacks = new Dictionary<string, Stack<string>>();
-
         static void Main()
         {
             // PartOne();
             PartTwo();
         }
 
+        static bool checkForDupes(string window)
+        {
+            for (int i = 0; i < window.Length; i++)
+            {
+                string letter = window[i].ToString();
+                if (window.Substring(i + 1).Contains(letter))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
         static void PartOne()
         {
             // string input = System.IO.File.ReadAllText(@"test.txt");
             string input = System.IO.File.ReadAllText(@"input.txt");
             // Console.WriteLine("Input:\n{0}", input);
             // Console.WriteLine("===========");
-
-            string[] parts = input.Split("\n\n");
-
-            string startingArrangement = parts[0];
-            string instructions = parts[1];
-
-            string[] layers = startingArrangement.Split("\n");
-            int length = layers.Length;
-            int columns = (layers[0].Length + 1) / 4;
-
-            for (int index = 0; index < columns; index++)
+            int i = 0;
+            string window = "";
+            while (i < input.Length)
             {
-                Stack<string> stack = new Stack<string>();
-                stacks.Add((index + 1).ToString(), stack);
-            }
-
-            for (int rowIndex = length - 2; rowIndex >= 0; rowIndex--)
-            {
-                string layer = layers[rowIndex];
-                for (int colIndex = 0; colIndex < columns; colIndex++)
+                string letter = input[i].ToString();
+                Console.WriteLine("i: {0} window: {1} letter: {2}", i, window, letter);
+                if (window.Length == 4)
                 {
-                    string container = layer.Substring((((colIndex + 1) * 4) - 3), 1).Trim();
-                    if (container != "")
+                    window = window.Substring(1);
+                    Console.WriteLine("After removal window: {0}", window);
+                }
+                window += letter;
+                Console.WriteLine("After adding letter window: {0}", window);
+                if (window.Length == 4)
+                {
+                    if (!checkForDupes(window))
                     {
-                        stacks[(colIndex + 1).ToString()].Push(container);
+                        Console.WriteLine("Found it at {0}! {1}", i + 1, window);
+                        return;
                     }
                 }
-            }
-            for (int colIndex = 0; colIndex < columns; colIndex++)
-            {
-                Console.WriteLine("stack {0}: {1}", colIndex, string.Join(',', stacks[(colIndex + 1).ToString()].ToArray()));
-            }
-            Console.WriteLine("*********");
-            string[] steps = instructions.Split("\n");
-            foreach (string step in steps)
-            {
-                string[] stepParts = step.Split(" ");
-                int quantity = int.Parse(stepParts[1]);
-                string startColumn = stepParts[3];
-                string endColumn = stepParts[5];
-                for (int counter = 0; counter < quantity; counter++)
-                {
-                    Stack<string> fromStack = stacks[startColumn];
-                    Stack<string> toStack = stacks[endColumn];
-                    string container = fromStack.Pop();
-                    toStack.Push(container);
-                    for (int colIndex = 0; colIndex < columns; colIndex++)
-                    {
-                        Console.WriteLine("stack {0}: {1}", colIndex, string.Join(',', stacks[(colIndex + 1).ToString()].ToArray()));
-                    }
-                    Console.WriteLine("---");
-                }
-            }
-            for (int colIndex = 0; colIndex < columns; colIndex++)
-            {
-                Console.WriteLine(stacks[(colIndex + 1).ToString()].Pop());
+                i++;
             }
         }
 
@@ -85,68 +61,25 @@ namespace com.thomasqbrady
             string input = System.IO.File.ReadAllText(@"input.txt");
             // Console.WriteLine("Input:\n{0}", input);
             // Console.WriteLine("===========");
-
-            string[] parts = input.Split("\n\n");
-
-            string startingArrangement = parts[0];
-            string instructions = parts[1];
-
-            string[] layers = startingArrangement.Split("\n");
-            int length = layers.Length;
-            int columns = (layers[0].Length + 1) / 4;
-
-            for (int index = 0; index < columns; index++)
+            int i = 0;
+            string window = "";
+            while (i < input.Length)
             {
-                Stack<string> stack = new Stack<string>();
-                stacks.Add((index + 1).ToString(), stack);
-            }
-
-            for (int rowIndex = length - 2; rowIndex >= 0; rowIndex--)
-            {
-                string layer = layers[rowIndex];
-                for (int colIndex = 0; colIndex < columns; colIndex++)
+                string letter = input[i].ToString();
+                if (window.Length == 14)
                 {
-                    string container = layer.Substring((((colIndex + 1) * 4) - 3), 1).Trim();
-                    if (container != "")
+                    window = window.Substring(1);
+                }
+                window += letter;
+                if (window.Length == 14)
+                {
+                    if (!checkForDupes(window))
                     {
-                        stacks[(colIndex + 1).ToString()].Push(container);
+                        Console.WriteLine("Found it at {0}! {1}", i + 1, window);
+                        return;
                     }
                 }
-            }
-            for (int colIndex = 0; colIndex < columns; colIndex++)
-            {
-                Console.WriteLine("stack {0}: {1}", colIndex, string.Join(',', stacks[(colIndex + 1).ToString()].ToArray()));
-            }
-            Console.WriteLine("*********");
-            string[] steps = instructions.Split("\n");
-            foreach (string step in steps)
-            {
-                string[] stepParts = step.Split(" ");
-                int quantity = int.Parse(stepParts[1]);
-                Stack<string> hopper = new Stack<string>();
-                string startColumn = stepParts[3];
-                string endColumn = stepParts[5];
-                for (int counter = 0; counter < quantity; counter++)
-                {
-                    Stack<string> fromStack = stacks[startColumn];
-                    string container = fromStack.Pop();
-                    hopper.Push(container);
-                }
-                for (int counter = 0; counter < quantity; counter++)
-                {
-                    Stack<string> toStack = stacks[endColumn];
-                    string container = hopper.Pop();
-                    toStack.Push(container);
-                }
-                for (int colIndex = 0; colIndex < columns; colIndex++)
-                {
-                    Console.WriteLine("stack {0}: {1}", colIndex, string.Join(',', stacks[(colIndex + 1).ToString()].ToArray()));
-                }
-                Console.WriteLine("---");
-            }
-            for (int colIndex = 0; colIndex < columns; colIndex++)
-            {
-                Console.WriteLine(stacks[(colIndex + 1).ToString()].Pop());
+                i++;
             }
         }
     }
