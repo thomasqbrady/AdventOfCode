c0fa2b1a7a7aa95417ec8369195edbf341ac6339
Thomas Q Brady <thbrady@zynga.com>
2022-12-08 15:06:35 -0600

    through day8

--- 2022/day8/Program.cs
+++ 2022/day8/Program.cs
@@ -8,8 +8,8 @@ namespace com.thomasqbrady
     {
         static void Main()
         {
-            PartOne();
-            // PartTwo();
+            // PartOne();
+            PartTwo();
         }
 
         static int[,] make2DArray(string[] rows)
@@ -35,46 +35,123 @@ namespace com.thomasqbrady
             return array2D;
         }
 
-        static List<string> check(int[] direction, List<string> candidates, int[,] array2D, int length)
-        {
+        static bool checkPath(int[] direction, string candidate, int[,] array2D, int length) {
             int rise = direction[1];
             int run = direction[0];
-            List<string> toRemove = new List<string>();
-            foreach (string candidateAddress in candidates)
+            string[] bits = candidate.Split(":");
+            int x = int.Parse(bits[0]);
+            int y = int.Parse(bits[1]);
+            // Console.WriteLine("candidate: {0}:{1} is {2}", x, y, array2D[x, y]);
+            int candidateHeight = array2D[x, y];
+            bool visible = true;
+            while (x > 0 && x < length - 1 && y > 0 && y < length - 1)
             {
-                string[] bits = candidateAddress.Split(":");
-                int x = int.Parse(bits[0]);
-                int y = int.Parse(bits[1]);
-                // Console.WriteLine("candidate: {0}:{1} is {2}", x, y, array2D[x, y]);
-                int candidateHeight = array2D[x, y];
-                while (x > 0 && x < length - 1 && y > 0 && y < length - 1)
+                x += run;
+                y += rise;
+                // Console.WriteLine("x: {0} y: {1}", x, y);
+                int comparatorHeight = array2D[x, y];
+                if (comparatorHeight >= candidateHeight)
                 {
-                    x += run;
-                    y += rise;
-                    // Console.WriteLine("x: {0} y: {1}", x, y);
-                    int comparatorHeight = array2D[x, y];
-                    if (comparatorHeight >= candidateHeight)
-                    {
-                        toRemove.Add(candidateAddress);
-                        // Console.WriteLine("Candidate {0} will be removed", candidateAddress);
-                    }
+                    visible = false;
+                    // Console.WriteLine("Candidate {0} will be removed", candidateAddress);
                 }
             }
-            foreach (string candidateAddress in toRemove)
+            return visible;
+        }
+
+        static List<string> check(List<string> candidates, int[,] array2D, int length)
+        {
+            int[] up = { 0, -1 };
+            int[] right = { 1, 0 };
+            int[] down = { 0, 1 };
+            int[] left = { -1, 0 };
+            List<string> toRemove = new List<string>();
+            foreach (string candidate in candidates)
             {
-                // Console.WriteLine("Removing {0}", candidateAddress);
-                candidates.Remove(candidateAddress);
+                bool visibleFromNorth = checkPath(up, candidate, array2D, length);
+                bool visibleFromEast = checkPath(right, candidate, array2D, length);
+                bool visibleFromSouth = checkPath(down, candidate, array2D, length);
+                bool visibleFromWest = checkPath(left, candidate, array2D, length);
+                if (!(visibleFromNorth || visibleFromEast || visibleFromSouth || visibleFromWest)) {
+                    toRemove.Add(candidate);
+                }
+            }
+            foreach (string candidate in toRemove)
+            {
+                Console.WriteLine("Removing {0}", candidate);
+                candidates.Remove(candidate);
             }
             return candidates;
         }
 
         static void PartOne()
         {
-            string input = System.IO.File.ReadAllText(@"test.txt");
-            // string input = System.IO.File.ReadAllText(@"input.txt");
+            // string input = System.IO.File.ReadAllText(@"test.txt");
+            string input = System.IO.File.ReadAllText(@"input.txt");
             // Console.WriteLine("Input:\n{0}", input);
             // Console.WriteLine("===========");
             string[] rows = input.Split("\n");
+            int length = rows.Length;
+            int[,] array2D = make2DArray(rows);
+            List<string> candidates = new List<string>();
+            for (int x = 1; x < length - 1; x++)
+            {
+                for (int y = 1; y < length - 1; y++)
+                {
+                    string address = "";
+                    address += x;
+                    address += ":";
+                    address += y;
+                    candidates.Add(address);
+                    // Console.WriteLine("Candidate: {0}", address);
+                }
+            }
+            Console.WriteLine("BEFORE");
+            foreach (var candidate in candidates)
+            {
+                Console.WriteLine(candidate);
+            }
+            candidates = check(candidates, array2D, length);
+            Console.WriteLine("AFTER");
+            foreach (var candidate in candidates)
+            {
+                Console.WriteLine(candidate);
+            }
+            int visibleTrees = (length - 1)*4 + candidates.Count;
+            Console.WriteLine("Number of visible trees: {0}", visibleTrees);
+        }
+
+        static int checkDistance(int[] direction, string candidate, int[,] array2D, int length) {
+            int rise = direction[1];
+            int run = direction[0];
+            string[] bits = candidate.Split(":");
+            int x = int.Parse(bits[0]);
+            int y = int.Parse(bits[1]);
+            // Console.WriteLine("candidate: {0}:{1} is {2}", x, y, array2D[x, y]);
+            int candidateHeight = array2D[x, y];
+            int distance = 0;
+            while (x > 0 && x < length - 1 && y > 0 && y < length - 1)
+            {
+                x += run;
+                y += rise;
+                // Console.WriteLine("x: {0} y: {1}", x, y);
+                int comparatorHeight = array2D[x, y];
+                distance++;
+                if (candidateHeight <= comparatorHeight) {
+                    break;
+                }
+
+            }
+            return distance;
+        }
+
+        static void PartTwo()
+        {
+            // string input = System.IO.File.ReadAllText(@"test.txt");
+            string input = System.IO.File.ReadAllText(@"input.txt");
+            Console.WriteLine("Input:\n{0}", input);
+            Console.WriteLine("===========");
+             string[] rows = input.Split("\n");
             int length = rows.Length;
             int[,] array2D = make2DArray(rows);
             List<string> candidates = new List<string>();
@@ -94,25 +171,23 @@ namespace com.thomasqbrady
             int[] right = { 1, 0 };
             int[] down = { 0, 1 };
             int[] left = { -1, 0 };
-            Console.WriteLine("BEFORE UP");
-            foreach (var candidate in candidates)
+            int highestScore = 0;
+            string winner = "";
+            List<string> toRemove = new List<string>();
+            foreach (string candidate in candidates)
             {
-                Console.WriteLine(candidate);
+                int visibilityToNorth = checkDistance(up, candidate, array2D, length);
+                int visibilityToEast = checkDistance(right, candidate, array2D, length);
+                int visibilityToSouth = checkDistance(down, candidate, array2D, length);
+                int visibilityToWest = checkDistance(left, candidate, array2D, length);
+                int score = visibilityToNorth * visibilityToEast * visibilityToSouth * visibilityToWest;
+                Console.WriteLine("Candidate {0} scores N: {1} E: {2} S: {3} W: {4} total: {5}", candidate, visibilityToNorth, visibilityToEast, visibilityToSouth, visibilityToWest, score);
+                if (score > highestScore) {
+                    highestScore = score;
+                    winner = candidate;
+                }
+                Console.WriteLine("Winner is {0} at {1}", winner, highestScore);
             }
-            candidates = check(up, candidates, array2D, length);
-            Console.WriteLine("AFTER");
-            foreach (var candidate in candidates)
-            {
-                Console.WriteLine(candidate);
-            }
-        }
-
-        static void PartTwo()
-        {
-            string input = System.IO.File.ReadAllText(@"test.txt");
-            // string input = System.IO.File.ReadAllText(@"input.txt");
-            Console.WriteLine("Input:\n{0}", input);
-            Console.WriteLine("===========");
-        }
+      }
     }
 }
