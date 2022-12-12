ac33f5b0bdd949b61d8a6fcccf007f7b4adf23bd
Thomas Q Brady <121729+thomasqbrady@users.noreply.github.com>
2022-12-12 14:54:23 +0000

    started day12

---
+++ 2022/day12/Program.cs
@@ -0,0 +1,55 @@
+﻿/** author: Thomas Q Brady
+*** Advent of Code 2022 day 1
+**/
+
+namespace com.thomasqbrady
+{
+    class Day
+    {
+        static void Main()
+        {
+            PartOne();
+            // PartTwo();
+        }
+        static void PartOne()
+        {
+            string input = System.IO.File.ReadAllText(@"test.txt");
+            // string input = System.IO.File.ReadAllText(@"input.txt");
+            Console.WriteLine("Input:\n{0}", input);
+            Console.WriteLine("===========");
+            
+            using (StringReader reader = new StringReader(input))
+            {
+                int rowIndex = 0;
+                string? legendRow = String.Empty;
+                do {
+                    legendRow = reader.ReadLine();
+                    if (legendRow != null) {
+                        char[] positions = legendRow.ToCharArray();
+                        int colIndex = 0;
+                        foreach (char position in positions) {
+                            switch (char) {
+                                case 'S':
+                                    break;
+                                case 'E':
+                                    break'
+                                default :
+                                    break;
+                            }
+                            colIndex++;
+                        }
+                    }
+                    rowIndex++;
+                } while (legendRow != null);
+            }
+        }
+
+        static void PartTwo()
+        {
+            string input = System.IO.File.ReadAllText(@"test.txt");
+            // string input = System.IO.File.ReadAllText(@"input.txt");
+            Console.WriteLine("Input:\n{0}", input);
+            Console.WriteLine("===========");
+        }
+    }
+}
