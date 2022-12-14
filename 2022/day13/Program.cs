1c8e5d9a9875176b13ac1d460b783b59abc0f027
Thomas Q Brady <thbrady@zynga.com>
2022-12-14 15:44:02 -0600

    change to template, working on day 13

---
+++ 2022/day13/Program.cs
@@ -0,0 +1,47 @@
+﻿/** author: Thomas Q Brady
+*** Advent of Code 2022 day 1
+**/
+
+namespace com.thomasqbrady
+{
+    class Day
+    {
+        static void Main()
+        {
+            string input = System.IO.File.ReadAllText(@"test.txt");
+            // string input = System.IO.File.ReadAllText(@"input.txt");
+            Console.WriteLine("Input:\n{0}", input);
+            Console.WriteLine("===========");
+            string[] pairs = input.Split("\n\n");
+            PartOne(pairs);
+            // PartTwo();
+        }
+
+        static bool compare(string left, string right) {
+            return true;
+        }
+
+        static void PartOne(string[] pairs)
+        {
+            int score = 0;
+            for (int i = 0;i < pairs.Length;i++) {
+                string[] pair = pairs[i];
+                string left = pair[0];
+                string right = pair[1];
+                bool isOrdered = compare(left, right);
+                if (isOrdered) {
+                    score += i;
+                    Console.WriteLine("Pair {0} was ordered correctly, score tally: {1}", i, score);
+                }
+            }
+        }
+
+        static void PartTwo()
+        {
+            string input = System.IO.File.ReadAllText(@"test.txt");
+            // string input = System.IO.File.ReadAllText(@"input.txt");
+            Console.WriteLine("Input:\n{0}", input);
+            Console.WriteLine("===========");
+        }
+    }
+}
