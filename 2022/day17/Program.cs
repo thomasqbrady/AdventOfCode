a37b8854f2672a72ee5ffdba917afa4e70fa1ad0
Thomas Q Brady <thbrady@zynga.com>
2022-12-20 16:56:35 -0600

    partial day 17

--- 2022/day17/Program.cs
+++ 2022/day17/Program.cs
@@ -9,12 +9,14 @@ namespace com.thomasqbrady
         public int X;
         public int Y;
         public int Height;
+        public int Width;
         public int ShapeIndex;
 
-        public Shape(int x, int y, int height, int shapeIndex) {
+        public Shape(int x, int y, int height, int width, int shapeIndex) {
             this.X = x;
             this.Y = y;
             this.Height = height;
+            this.Width = width;
             this.ShapeIndex = shapeIndex;
         }
 
@@ -26,12 +28,13 @@ namespace com.thomasqbrady
     {
         static int shapeIndex = 0;
         static int chamberWidth = 7;
-        static int initialFallHeight = 2;
+        static int initialFallHeight = 3;
         static int initialX = 2;
-        static List<Shape> fallenShapes = new List<Shape>();
+        static IDictionary<(int, int), string> board = new Dictionary<(int, int), string>();
         static Shape? fallingShape = null;
         static string winds = "";
         static int windIndex = 0;
+        static int tallestPoint = 0;
 
         static string[][] shapes = new string[][] {
             new string[] {"####"},
@@ -55,26 +58,57 @@ namespace com.thomasqbrady
             }
         }
 
-        static int windStringToCoordinateDeltas(string wind) {
+        static int windStringToCoordinateDeltas(char wind) {
             switch (wind) {
-                case "<":
+                case '<':
                     return -1;
                 default :
                     return 1;
             }
         }
 
-        static void spawnShape(int Y) {
-            string[] shapeString = shapes[shapeIndex];
-            int shapeHeight = shapeString.Length;
-            fallingShape = new Shape(initialX, Y + shapeHeight, shapeHeight, shapeIndex);
+        static Shape spawnShape(int Y) {
+            string[] shapeStrings = shapes[shapeIndex];
+            int shapeHeight = shapeStrings.Length;
+            int shapeWidth = shapeStrings[0].Length;
+            Shape newShape = new Shape(initialX, Y + shapeHeight, shapeHeight, shapeWidth, shapeIndex);
             cycleShape();
-            Console.WriteLine(fallingShape);
+            Console.WriteLine(newShape);
+            return newShape;
         }
 
         static void print() {
-            List<(int, int)> map = new List<(int, int)>();
-            
+            int fallingY = (fallingShape != null) ? fallingShape.Y : 0;
+            int startY = (fallingY > tallestPoint) ? fallingY : tallestPoint;
+            for (int row = startY;row >= 0;row--) {
+                Console.Write("|");
+                for (int col = 0;col < chamberWidth;col++) {
+                    if (board.ContainsKey((col, row))) {
+                        if (row == 0) {
+                            Console.Write("-");
+                        } else {
+                            Console.Write("#");
+                        }
+                    } else {
+                        if (fallingShape != null) {
+                            if (row <= fallingShape.Y && row > fallingShape.Y - fallingShape.Height && col >= fallingShape.X && col < fallingShape.X + fallingShape.Width) {
+                                string[] shapeStrings = shapes[fallingShape.ShapeIndex];
+                                // Console.WriteLine($"Trying to show shapeStrings {Math.Abs(fallingShape.Y - row)}:{Math.Abs(fallingShape.X - col)}");
+                                if (shapeStrings[Math.Abs(fallingShape.Y - row)][col - fallingShape.X] == '#') {
+                                    Console.Write("#");
+                                } else {
+                                    Console.Write(" ");
+                                }
+                            } else {
+                                Console.Write(" ");
+                            }
+                        } else {
+                            Console.Write(" ");
+                        }
+                    }
+                }
+                Console.WriteLine("|");
+            }
         }
 
         static void LogObject(object o) {
@@ -93,16 +127,69 @@ namespace com.thomasqbrady
 
         static void PartOne(string input)
         {
-            int fallFrom = initialFallHeight;
-            for (int i = 0;i < 2022;i++) {
+            for (int i = 0;i < chamberWidth;i++) {
+                board[(i, 0)] = "#";
+            }
+            winds = input;
+            // for (int i = 0;i < 2022;i++) {
+            for (int i = 0;i < 4;i++) {
                 if (fallingShape == null) {
-                    spawnShape(fallFrom);
+                    fallingShape = spawnShape(tallestPoint + initialFallHeight);
                 }
-                // while (fallingShape != null) {
-                //     string wind = winds[windIndex];
-                //     cycleWind();
-
-                // }
+                Console.WriteLine("Start of round");
+                print();
+                while (fallingShape != null) {
+                    char wind = winds[windIndex];
+                    int delta = windStringToCoordinateDeltas(wind);
+                    int nextX = fallingShape.X + delta;
+                    // have to check whole shape, not just left and right of top row...
+                    if (board.ContainsKey((nextX, fallingShape.Y)) || board.ContainsKey((nextX + fallingShape.Width, fallingShape.Y)) || nextX < 0 || nextX + fallingShape.Width > chamberWidth) {
+                        Console.WriteLine($"Cannot blow to {nextX}:{fallingShape.Y}");
+                    } else {
+                        Console.WriteLine($"Shape blows to {nextX}:{fallingShape.Y}");
+                        fallingShape.X = nextX;
+                    }
+                    Console.WriteLine("After blowing");
+                    print();
+                    int nextY = fallingShape.Y - 1;
+                    string[] shapeStrings = shapes[fallingShape.ShapeIndex];
+                    bool wouldCollide = false;
+                    for (int row = 0;row < shapeStrings.Length;row++) {
+                        for (int col = 0;col < shapeStrings[row].Length;col++) {
+                            if (shapeStrings[row][col] == '#') {
+                                Console.WriteLine($"Shape located at {fallingShape.X}:{fallingShape.Y} has a # at {fallingShape.X + col}:{fallingShape.Y - row}");
+                                Console.WriteLine($"Checking to see if there is something for it to collide with at {fallingShape.X + col}:{nextY - row}");
+                                (int, int) placeToCheck = (fallingShape.X + col, nextY - row);
+                                if (board.ContainsKey(placeToCheck)) {
+                                    Console.WriteLine("  will collide");
+                                    wouldCollide = true;
+                                } else {
+                                    Console.WriteLine("  clear");
+                                }
+                            }
+                        }
+                    }
+                    if (wouldCollide) {
+                        Console.WriteLine("shape should stop falling");
+                        for (int row = 0;row < shapeStrings.Length;row++) {
+                            for (int col = 0;col < shapeStrings[row].Length;col++) {
+                                if (shapeStrings[row][col] == '#') {
+                                    board[(fallingShape.X + col, fallingShape.Y - row)] = "#";
+                                }
+                            }
+                        }
+                        if (fallingShape.Y > tallestPoint) {
+                            tallestPoint = fallingShape.Y;
+                        }
+                        fallingShape = null;
+                    } else {
+                        fallingShape.Y = nextY;
+                        Console.WriteLine($"Shape falls to {fallingShape.X}:{fallingShape.Y}");
+                    }
+                    cycleWind();
+                    Console.WriteLine("End of round");
+                    print();
+                }
             }
         }
 
